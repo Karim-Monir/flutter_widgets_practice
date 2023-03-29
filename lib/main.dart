1d0003d1704a874659dd16d0c2b49c2bf26419be
Karim Monir <karim.monir1992@gmail.com>
2023-03-29 20:57:27 +0200

    Slight changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_practice/pages/curved_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NavBar(),
+      home: const NavBar(),
     );
   }
 }
